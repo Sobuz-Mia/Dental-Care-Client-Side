18ae44eb358c489a3582fbb2e22f63dc6d09892c
Sobuz <sobuz.info0298@gmail.com>
2024-02-06 21:01:04 +0600

    layout and routes setup done

---
+++ src/layout/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Home/Navbar/Navbar";
+
+const Main = () => {
+  return (
+    <div className="max-w-7xl mx-auto bg-[white]">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Main;
