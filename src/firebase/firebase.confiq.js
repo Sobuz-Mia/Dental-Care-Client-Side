f6792da178fe7a9b65ca13f2a090e7a695bbf4c7
Sobuz <sobuz.info0298@gmail.com>
2024-03-08 21:20:57 +0600

    firebase intregation in our project

---
+++ src/firebase/firebase.confiq.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.apiKey,
+  authDomain: import.meta.env.authDomain,
+  projectId: import.meta.env.projectId,
+  storageBucket: import.meta.env.storageBucket,
+  messagingSenderId: import.meta.env.messagingSenderId,
+  appId: import.meta.env.appId,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
